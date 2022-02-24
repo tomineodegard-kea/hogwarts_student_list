89300ff3d6bc9dda1bdb0ccbc2555e174156a370
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-24 12:30:23 +0100

    started fetching the json and creating the student object

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
 "use strict";
+let allStudents = [];
 
-window.addEventListener("DOMContentLoaded", loadPage);
+window.addEventListener("DOMContentLoaded", start);
 
-function loadPage() {}
+// ________ START ________
+function start() {
+  const filterBtn = document.querySelectorAll("[data-action=filter]");
 
-// ________ LOOP VIEW ________
+  filterBtn.forEach((btn) => {
+    btn.addEventListener("click", buildList);
+  });
+
+  getStudentsJSON();
+}
+
+// -------- getting all the students by fetching json
+async function getStudentsJSON() {
+  const studentsUrl = "https://petlatkea.dk/2021/hogwarts/students.json";
+  let studentsData = await fetch(studentsUrl);
+  let studentsJSON;
+  studentsJSON = await studentsData.json();
+
+  createStudentsFromJSON();
+}
+
+// -------- getting all the bloodtypes by fetching json
+async function getBloodJSON() {
+  const bloodUrl = "https://petlatkea.dk/2021/hogwarts/families.json";
+  let bloodData = await fetch(bloodUrl);
+  let bloodStudents;
+  bloodStudents = await bloodData.json();
+
+  // createStudentsFromJSON();
+}
+
+// -------- CREATE THE STUDENT OBJECT --------
+function createStudentsFromJSON() {
+  allStudents.forEach((object) => {
+    const Student = {
+      firstName: "",
+      lastName: "",
+      house: "",
+    };
+
+    // create a objects from a prototype
+    const studentObject = Object.create(Student);
+  });
+}
+
+function buildList() {
+  console.log("building list");
+}
+
 // -------- ALL FILTERING FUNCTIONS --------
 function filterGriffindor() {}
 function filterSlytherin() {}
 function filterHufflepuff() {}
 function filterRavenclaw() {}
+
+function filterExpelled() {}
+function filterNonExpelled() {}
+function filterSquad() {}
+function filterBoys() {}
+function filterGirls() {}
+function filterPureblood() {}
+function filterHalfblood() {}
+function filterMuggle() {}
+
+function removeFilter() {}
 
 // -------- ALL SORTING FUNCTIONS --------
 function sortFistName() {}
